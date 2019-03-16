9015057dc0843913ef9bea5e415462be1deb15e5
valentinvstoyanov <valentinstoqnov98@gmail.com>
2019-03-16 08:20:24 +0200

    Add first 3 functions

--- homework/1-funs-and-queues/src/main/scala/homework1/Functions.scala
+++ homework/1-funs-and-queues/src/main/scala/homework1/Functions.scala
@@ -1,11 +1,31 @@
 package homework1
 
+import scala.annotation.tailrec
+
 object Functions {
-  def fromDigits(digits: List[Int], radix: Int = 10): Int = ???
+  def fromDigits(digits: List[Int], radix: Int = 10): Int = {
+    @tailrec def fromDigitsRec(digits: List[Int], acc: Int = 0): Int = digits match {
+      case Nil => acc
+      case head :: tail => fromDigitsRec(tail, acc + (head * math.pow(radix, tail.length).intValue()))
+    }
 
-  def parseInteger(integer: String, radix: Int = 10): Int = ???
+    fromDigitsRec(digits)
+  }
 
-  def zipMap(a: List[Int], b: List[Int], f: (Int, Int) => Int): List[Int] = ???
+  def parseInteger(integer: String, radix: Int = 10): Int = {
+    if (integer.head == '-') -parseInteger(integer.tail, radix)
+    else fromDigits(integer.toList.map(_.asDigit), radix)
+  }
+
+  def zipMap(a: List[Int], b: List[Int], f: (Int, Int) => Int): List[Int] = {
+    @tailrec def zipMapRec(a: List[Int], b: List[Int], acc: List[Int] = Nil): List[Int] = (a, b) match {
+      case (_, Nil) => acc.reverse
+      case (Nil, _) => acc.reverse
+      case (h1 :: t1, h2 :: t2) => zipMapRec(t1, t2, f(h1, h2) :: acc)
+    }
+
+    zipMapRec(a, b)
+  }
 
   def countCoinChangeVariants(denominations: List[Int], change: Int): Int = ???
 
--- homework/1-funs-and-queues/src/test/scala/homework1/FunctionsTest.scala
+++ homework/1-funs-and-queues/src/test/scala/homework1/FunctionsTest.scala
@@ -6,22 +6,61 @@ import Functions._
 class FunctionsTest extends FlatSpec with Matchers {
   "fromDigits" should "form a decimal number" in {
     fromDigits(List(1, 2, 3)) shouldBe 123
+    fromDigits(List(1, 0, 2, 3, 4, 5, 9)) shouldBe 1023459
+    fromDigits(List(0)) shouldBe 0
+    fromDigits(List(9, 9, 9, 9, 9)) shouldBe 99999
   }
 
   it should "form a hex number" in {
     fromDigits(List(1, 12, 4), 16) shouldBe 452
+    fromDigits(List(1, 0, 15, 9, 8, 7), 16) shouldBe 1112455
+    fromDigits(List(0), 16) shouldBe 0
+    fromDigits(List(15, 15, 15,15), 16) shouldBe 65535
+  }
+
+  it should "form an octal number" in {
+    fromDigits(List(1, 0, 5, 7, 1), 8) shouldBe 4473
+    fromDigits(List(0), 8) shouldBe 0
+    fromDigits(List(7, 7, 7, 7, 7), 8) shouldBe 32767
+  }
+
+  it should "form a binary number" in {
+    fromDigits(List(1, 0, 0, 0), 2) shouldBe 8
+    fromDigits(List(0), 2) shouldBe 0
+    fromDigits(List(1), 2) shouldBe 1
+    fromDigits(List(1, 1, 1), 2) shouldBe 7
   }
 
   "parseInteger" should "parse a decimal number" in {
     parseInteger("123") shouldBe 123
+    parseInteger("-45") shouldBe -45
+    parseInteger("0") shouldBe 0
+    parseInteger("9999") shouldBe 9999
+    parseInteger("456789") shouldBe 456789
   }
 
   it should "parse a hex number" in {
-    parseInteger("IC4", 16) shouldBe 452
+    parseInteger("1C4", 16) shouldBe 452
+    parseInteger("0", 16) shouldBe 0
+    parseInteger("ABCD", 16) shouldBe 43981
+    parseInteger("-ABCD", 16) shouldBe -43981
+    parseInteger("ZZZZZ", 16) shouldBe 2446675
+  }
+
+  it should "parse an octal number" in {
+    parseInteger("10571", 8) shouldBe 4473
+    parseInteger("-10571", 8) shouldBe -4473
+    parseInteger("0", 8) shouldBe 0
+    parseInteger("77777", 8) shouldBe 32767
   }
 
   "zipMap" should "transform two lists" in {
     zipMap(List(1, 2, 3), List(4, 5, 6), _ * _) shouldBe List(4, 10, 18)
+    zipMap(List(3, 6), List(20, 30, 40), (x, y) => y - x) shouldBe List(17, 24)
+    zipMap(List.range(1, 2001), List.range(2000, 4002), (x, y) => y - x) shouldBe List.fill(2000)(1999)
+    zipMap(Nil, Nil, _ + _) shouldBe Nil
+    zipMap(List(1, 2), Nil, _ + _) shouldBe Nil
+    zipMap(Nil, List(1, 2), _ + _) shouldBe Nil
   }
 
   "countCoinChangeVariants" should "count the ways to give a change" in {
